f86071c2136d33a96ccd3072bc02c8b7dcfdf1d6
Zoltan Szarvas <szarvas.zoltan@hotmail.com>
2024-07-22 23:20:18 +0000

    added EmailJS API with pop up form

--- assets/js/getmyplan.js
+++ assets/js/getmyplan.js
@@ -1,4 +1,4 @@
-
+// open popup window with form
 
 document.addEventListener("DOMContentLoaded", function() {
     const getmyplan = document.getElementById('getmyplan-btn');
@@ -9,3 +9,32 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 
 });
+
+
+// close button for pop up window
+const close = document.getElementById('close-btn');
+
+close.addEventListener("click", function() {
+    form.style.display = "none";
+})
+
+
+// EmailJS
+
+function sendMail(contactForm) {
+    emailjs.send("service_rraaj3w","hulkanasium",{
+        "from_name": contactForm.fname.value,
+        "message": contactForm.textarea.value,
+        "from_email": contactForm.email.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("ERROR", error);
+        }
+    )
+
+    return false;  // To block from loading a new page
+};
